f59cfb590ce666532d75a21b772caa61faf26aa3
emiuser <62060395+emiuser@users.noreply.github.com>
2022-04-18 15:53:00 -0300

    se agregaron las imagenes en un assets

--- src/containers/mock.js
+++ src/containers/mock.js
@@ -1,27 +1,41 @@
+/** Woman */
+import laVidaEsBella from '../assets/woman/la-vida-es-bella.jpg';
+import veryIrresistible from '../assets/woman/very-irresistible.jpg';
+import scandalPink from '../assets/woman/scandal-pink.jpg';
+import kenzoFlowers from '../assets/woman/kenzo-flowers.jpg';
+import isseyMiyakePlata from '../assets/woman/issey-miyake-plata.jpg';
+import halloween from '../assets/woman/halloween.jpg';
+
+/** Man */
+import pureXsBlue from '../assets/man/pure-xs-blue-men.jpg';
+import backXs from '../assets/man/black-xs-men.jpg';
+import PoloRed from '../assets/man/polo-red.jpg';
+import OneMillion from '../assets/man/one_million.jpg';
+
 export const imagesWoman = [
     {
         label: 'La vida es bella',
-        imgPath: 'https://scontent.faep9-3.fna.fbcdn.net/v/t39.30808-6/278026810_10227515921148646_3269883686207146471_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=FUccaIcv6D4AX8adIp4&_nc_ht=scontent.faep9-3.fna&oh=00_AT_XF7Vc-cxRafKmgTP0tg71PMBrUASwUyMwoL9yjncRDw&oe=62511252',
+        imgPath: laVidaEsBella,
     },
     {
         label: 'Very Irresistible',
-        imgPath: 'https://scontent.faep9-1.fna.fbcdn.net/v/t39.30808-6/278006845_10227515921108645_150147316116785675_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=730e14&_nc_ohc=AK9sTDZReVcAX8nnpbl&tn=iqLrGP8E6nyeOmyD&_nc_ht=scontent.faep9-1.fna&oh=00_AT8X368q2GZhxpCKXkrdf87Cj_vF4uxEASm6BMMgn7Vljw&oe=6251A389',
+        imgPath: veryIrresistible,
     },
     {
         label: 'Scandal Pink',
-        imgPath: 'https://scontent.faep9-3.fna.fbcdn.net/v/t39.30808-6/277788039_10227515921268649_3215878386627479172_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=xLCy1ZfZ8HkAX94e_Ib&_nc_ht=scontent.faep9-3.fna&oh=00_AT8jVi3kVcuMPm3Xo8N4U4GDtbcZBT_AA7b1iktThoihIg&oe=625119C2',
+        imgPath: scandalPink,
     },
     {
         label: 'Kenzo Flower',
-        imgPath: 'https://scontent.faep9-1.fna.fbcdn.net/v/t39.30808-6/278023685_10227515948109320_2739117264306494045_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=730e14&_nc_ohc=V2wXsdXGm9UAX_F1sWE&_nc_ht=scontent.faep9-1.fna&oh=00_AT8OYgQ44yyzzAqV9DDDPUGCcrsBR4Vs6zyqsM5pTxvqYg&oe=6251FA18',
+        imgPath: kenzoFlowers,
     },
     {
         label: 'Issey Miyake Plata',
-        imgPath: 'https://scontent.faep9-1.fna.fbcdn.net/v/t39.30808-6/278038375_10227515965109745_3421943747307131571_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_ohc=-U_qrmzrQBUAX_BUPzI&tn=iqLrGP8E6nyeOmyD&_nc_ht=scontent.faep9-1.fna&oh=00_AT-0fYD5rBc_C8x5tRkcuqEFYoyCuicb55UyhInzAxoY4A&oe=625121C4',
+        imgPath: isseyMiyakePlata,
     },
     {
         label: 'Hallowen',
-        imgPath: 'https://scontent.faep9-1.fna.fbcdn.net/v/t39.30808-6/278005501_10227516002990692_4865706851210692378_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_ohc=BH4vCvCOOHEAX_1epGO&tn=iqLrGP8E6nyeOmyD&_nc_ht=scontent.faep9-1.fna&oh=00_AT8jRGfahQ__OVjwNRHPo-S0ScVlu7ICfk08pDq3NESuJQ&oe=62514107',
+        imgPath: halloween,
     },
 ];
 
@@ -29,18 +43,18 @@ export const imagesMan = [
     /** Hombre */
     {
         label: 'Pure XS',
-        imgPath: 'https://scontent.faep9-3.fna.fbcdn.net/v/t39.30808-6/278013370_10227515987910315_2361567233488923897_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=xOTgK6zfueIAX_l33q9&tn=iqLrGP8E6nyeOmyD&_nc_ht=scontent.faep9-3.fna&oh=00_AT9xnfu5DtsNKtL4U4_vcZ3KsHTzCFjUZ0XrgxN4IHEm7A&oe=6252306F',
+        imgPath: pureXsBlue,
     },
     {
         label: 'Black XS Men',
-        imgPath: 'https://scontent.faep9-1.fna.fbcdn.net/v/t39.30808-6/277994524_10227516032151421_5200993095543448316_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_ohc=pS5I8FGxcP8AX-cFiGw&_nc_ht=scontent.faep9-1.fna&oh=00_AT9pXjcb8iFb0AUx0gzQUiSgjOpmiXzzTZk0LlHVmNC7Ig&oe=6252214C',
+        imgPath: backXs,
     },
     {
         label: 'Polo Red',
-        imgPath: 'https://scontent.faep9-2.fna.fbcdn.net/v/t39.30808-6/278060351_10227516037791562_1544099971697058669_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=o_cyOZWScMgAX_rzqen&_nc_ht=scontent.faep9-2.fna&oh=00_AT9OrqgXuQrxMLZg9HFH_O43O72Avno2Jlf4u03hIlf-DA&oe=6250F84B',
+        imgPath: PoloRed,
     },
     {
         label: 'Million',
-        imgPath: 'https://scontent.faep9-3.fna.fbcdn.net/v/t39.30808-6/278018144_10227516050271874_4322194071185278828_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=730e14&_nc_ohc=McCsIiKT4lEAX9uuxLw&tn=iqLrGP8E6nyeOmyD&_nc_ht=scontent.faep9-3.fna&oh=00_AT8lrVyeaLGbmqOAxQJF-HWnTVBNVrFyHRp1lMN0ATUmlw&oe=6251654F',
+        imgPath: OneMillion,
     },
 ];
